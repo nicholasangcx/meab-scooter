23a53e72e722cdaa53262ea5df68d25bb69ab4af
Ang Nicholas <nicholas@Angs-MacBook-Pro.local>
2019-09-10 01:28:08 +0800

    initial commit

---
+++ flask_backend/static/react/precache-manifest.df83da5d84c0cd7ded3e73190758a112.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c463d88069b26c17d5a9ed77d917196",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "81633c5ea05ee3a748d0",
+    "url": "/static/react/css/main.9fc6961a.chunk.css"
+  },
+  {
+    "revision": "59c1cb0988114e289e57",
+    "url": "/static/react/js/2.dddcd64c.chunk.js"
+  },
+  {
+    "revision": "81633c5ea05ee3a748d0",
+    "url": "/static/react/js/main.a599c386.chunk.js"
+  },
+  {
+    "revision": "ae70a58b34ba726577db",
+    "url": "/static/react/js/runtime~main.9353bfcb.js"
+  }
+]);
